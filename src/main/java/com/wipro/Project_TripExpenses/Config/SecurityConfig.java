e16332e0ae6f7a05a80687259d5604838a3301b4
vyshnavikotala <vyshnavireddyk2000@gmail.com>
2024-06-03 18:18:17 +0530

    Update SecurityConfig.java

--- src/main/java/com/wipro/Project_TripExpenses/Config/SecurityConfig.java
+++ src/main/java/com/wipro/Project_TripExpenses/Config/SecurityConfig.java
@@ -1,54 +1,48 @@
 package com.wipro.Project_TripExpenses.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import com.wipro.Project_TripExpenses.Repository.UserRepo;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true,jsr250Enabled = true)
-
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private UserRepo userRepo;
 
     @Bean
     //authentication
     public UserDetailsService userDetailsService() {
-        return new UserInfoUserDetailsService();
+        return new UserInfoUserDetailsService(userRepo);
     }
-/* @Bean
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        return http.csrf().disable()
+                .authorizeHttpRequests()
                 .requestMatchers("/api/register", "/api/login").permitAll()
                 .requestMatchers("/api/**").authenticated()
                 .and()
                 .formLogin()
                 .and()
                 .build();
-
-        return http.build();
-    }*/
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/register","/api/login").permitAll()
-                .and()
-                .authorizeHttpRequests().requestMatchers("/api/**")
-                .authenticated().and().formLogin().and().build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-
         return new BCryptPasswordEncoder();
     }
 
@@ -61,3 +55,5 @@ public class SecurityConfig {
     }
 
 }
+
+                
