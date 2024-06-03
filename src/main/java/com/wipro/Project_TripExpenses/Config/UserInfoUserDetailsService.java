b5f9585914b8f06dfef390d22639f72af2d2b2fc
vyshnavikotala <vyshnavireddyk2000@gmail.com>
2024-06-03 18:20:58 +0530

    Update UserInfoUserDetailsService.java

--- src/main/java/com/wipro/Project_TripExpenses/Config/UserInfoUserDetailsService.java
+++ src/main/java/com/wipro/Project_TripExpenses/Config/UserInfoUserDetailsService.java
@@ -1,27 +1,38 @@
 package com.wipro.Project_TripExpenses.Config;
 
 
+import com.wipro.Project_TripExpenses.Model.User;
 import com.wipro.Project_TripExpenses.Repository.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import com.wipro.Project_TripExpenses.Model.User;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class UserInfoUserDetailsService implements UserDetailsService {
-    @Autowired
-    private UserRepo repo;
+
+    private final UserRepo userRepo;
+
+    public UserInfoUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> userInfo = repo.findByUsername(username);
-        return userInfo.map(UserInfoUserDetails::new)
-                .orElseThrow(() -> new UsernameNotFoundException("user not found " + username));
-
+        Optional<User> user = userRepo.findByUsername(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        List<GrantedAuthority> authorities = Arrays.stream(user.get().getRole().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(user.get().getUsername(), user.get().getPassword(), authorities);
     }
 }
